d543ede2d266fe23ede14e6e3818b2b9b2d69f13
jacob-tucker <jacobtucker818@gmail.com>
2022-07-25 11:28:28 -0400

    meta tag

---
+++ src/lib/components/utilities/Meta.svelte
@@ -0,0 +1,37 @@
+<script>
+  export let title;
+  export let url;
+</script>
+
+<svelte:head>
+  <title>Send a Geeft on Flow</title>
+  <meta
+    name="description"
+    content={"Send a Geeft filled with NFTs and Tokens on Flow"} />
+  <meta name="author" content={"Emerald City"} />
+  <meta
+    property="og:image"
+    content={"http://geeft.ecdao.org/social-card.png"} />
+  <meta
+    property="twitter:image"
+    content={"http://geeft.ecdao.org/social-card.png"} />
+  <meta name="twitter:card" content={"summary_large_image"} />
+  <meta property="og:title" content={"Send a Geeft on Flow"} />
+  <meta property="og:type" content="article" />
+  <meta property="og:url" content={"https://geeft.ecdao.org/"} />
+
+  <!--  Non-Essential, But Recommended -->
+  <meta
+    property="og:description"
+    content={"Send a Geeft filled with NFTs and Tokens on Flow"} />
+  <meta property="og:site_name" content="Geeft - a gifting platform on Flow" />
+  <meta name="twitter:image:alt" content="Geeft - a gifting platform on Flow" />
+
+  <meta name="twitter:title" content="Send a Geeft on Flow" />
+  <meta
+    name="twitter:description"
+    content={"Send a Geeft filled with NFTs and Tokens on Flow"} />
+
+  <!--  Non-Essential, But Required for Analytics -->
+  <meta name="twitter:site" content="@emerald_dao" />
+</svelte:head>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,12 @@
 <script>
   import Nav from "$lib/components/Nav.svelte";
+  import Meta from "$lib/components/utilities/Meta.svelte";
   import Transaction from "$lib/components/utilities/Transaction.svelte";
 
   import "../app.css";
 </script>
+
+<Meta />
 
 <Nav />
 <main>

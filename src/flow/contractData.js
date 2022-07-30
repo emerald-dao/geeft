a015e81b6025cd7621d3a4c745c77644c8a74d21
jacob-tucker <jacobtucker818@gmail.com>
2022-07-30 14:04:56 -0400

    mainnet time lads

--- src/flow/contractData.js
+++ src/flow/contractData.js
@@ -118,7 +118,7 @@ export const contractData = {
       networks: {
         emulator: "0xf8d6e0586b0a20c7",
         testnet: "",
-        mainnet: "0x47fa18bd1d39e427"
+        mainnet: "0x80ec5732ee129467"
       }
     },
     NonFungibleToken: {
